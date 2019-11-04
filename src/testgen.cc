96b1fb2b4b4cfc1d620059d7b5ad78f4bbaef3fe
Jeff Garzik <jeff@bloq.com>
2019-11-04 01:41:37 -0500

    testgen: add new "empty" test plan

--- src/testgen.cc
+++ src/testgen.cc
@@ -11,6 +11,7 @@
 #include <vector>
 #include <stdio.h>
 #include <unistd.h>
+#include <cassert>
 #include <time.h>
 #include <ctype.h>
 #include <gdbm.h>
@@ -25,7 +26,48 @@ public:
 	string value;
 };
 
-static int gen_test_data(string db_fn, string json_fn)
+static int gen_plan_empty(string db_fn, string json_fn)
+{
+	GDBM_FILE dbf = gdbm_open(db_fn.c_str(), 512, GDBM_NEWDB, 0666, NULL);
+	if (!dbf) {
+		fprintf(stderr, "gdbm_open failed\n");
+		return 1;
+	}
+
+	gdbm_count_t count = 0;
+	int rc = gdbm_count(dbf, &count);
+	if (rc) {
+		fprintf(stderr, "gdbm_count failed\n");
+		return 1;
+	}
+
+	assert(count == 0);
+
+	gdbm_close(dbf);
+
+	FILE *f = fopen(json_fn.c_str(), "w");
+	if (!f) {
+		fprintf(stderr, "fopen failed\n");
+		return 1;
+	}
+
+	time_t t = time(NULL);
+
+	fprintf(f, "{"
+	"  \"generated_by\":\"testgen\","
+	"  \"generated_time\":\"%lu\","
+	"  \"data_records\": %u,"
+	"  \"data\": [",
+		t,
+		0);
+
+	fprintf(f, "]}\n");
+	fclose(f);
+
+	return 0;
+}
+
+static int gen_plan_basic(string db_fn, string json_fn)
 {
 	vector<kv_pair> data;
 
@@ -42,7 +84,7 @@ static int gen_test_data(string db_fn, string json_fn)
 		data.push_back(p);
 	}
 
-	GDBM_FILE dbf = gdbm_open(db_fn.c_str(), 512, GDBM_WRCREAT, 0666, NULL);
+	GDBM_FILE dbf = gdbm_open(db_fn.c_str(), 512, GDBM_NEWDB, 0666, NULL);
 	if (!dbf) {
 		fprintf(stderr, "gdbm_open failed\n");
 		return 1;
@@ -96,12 +138,15 @@ static int gen_test_data(string db_fn, string json_fn)
 
 static void usage(const char *progname)
 {
-	fprintf(stderr, "Usage: %s -o output-db -j output-json\n", progname);
+	fprintf(stderr, "Usage: %s -o output-db -j output-json [options]\n", progname);
 	fprintf(stderr,
 "\n"
+"Required Options:\n"
+"\t-o DB-FILE\tOutput db\n"
+"\t-j JSON-FILE\tOutput JSON metadata to file\n"
 "Options:\n"
-"\t-o\tOutput db\n"
-"\t-j\tOutput JSON metadata to file\n"
+"\t-p PLAN\tGenerate according to test-plan PLAN\n"
+"\t\t\tAvailable plans: basic, empty\n"
 	);
 }
 
@@ -110,14 +155,18 @@ int main (int argc, char *argv[])
 	int opt;
 	char *out_fn = NULL;
 	char *out_json = NULL;
+	string plan = "basic";
 
-	while ((opt = getopt(argc, argv, "o:j:")) != -1) {
+	while ((opt = getopt(argc, argv, "o:j:p:")) != -1) {
 		switch (opt) {
 		case 'o':
 			out_fn = optarg;
 			break;
 		case 'j':
 			out_json = optarg;
+			break;
+		case 'p':
+			plan = optarg;
 			break;
 		default:
 			usage(argv[0]);
@@ -130,5 +179,11 @@ int main (int argc, char *argv[])
 		return 1;
 	}
 
-	return gen_test_data(out_fn, out_json);
+	if (plan == "basic")
+		return gen_plan_basic(out_fn, out_json);
+	if (plan == "empty")
+		return gen_plan_empty(out_fn, out_json);
+
+	fprintf(stderr, "Unknown test plan %s\n", plan.c_str());
+	return 1;
 }
